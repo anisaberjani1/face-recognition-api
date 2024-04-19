6abc81c8276be53ab945346082bea0b02326d463
anisaberjani1 <anisa.berjani@gmail.com>
2024-04-19 15:59:34 +0200

    Update on connection

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+        url: process.env.DEPLOYED_URL
+    //   host: process.env.DB_HOST,
+    //   port: process.env.DB_PORT,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
     },
 });
 
